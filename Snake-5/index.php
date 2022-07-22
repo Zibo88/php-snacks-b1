7f71ae768fa239234fdc68ce39bf177718dc273a
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-07-22 18:56:26 +0200

    Snake-5 array associativi, estrapolare dati ed inserirlo nel corretto riquadro

--- Snake-5/index.php
+++ Snake-5/index.php
@@ -34,56 +34,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Teachers and PM</title>
 </head>
 <body>
   
     <?php
-        $teacherArray = [];
-        $pmArray = [];
-
-        
+        // definisco gli elementi presenti nell'array $db in una varibile
+        // elementi presenti dentro teachers
         $teacherName = $db['teachers'];
         // var_dump($teacherName);
+        // elementi presenti dentro pm
         $pmName = $db['pm'];
         // var_dump($teacherLastname);
 
-
         // var_dump($db['pm'][0]['name']);
-        // var_dump($db['pm'][0]['lastname']);
-
-        for($i = 0; $i = $i < count($teacherName); $i++){
-            var_dump($teacherName);
-
-             if(!in_array($teacherName, $teacherArray)){
-
-                $teacherArray[] = $teacherName;
-                var_dump($teacherArray);
-             }
-        } 
-
-        for($i = 0; $i = $i < count($pmName); $i++){
-            var_dump($pmName);
-
-             if(!in_array($pmName, $pmArray)){
-
-                $pmArray[] = $pmName;
-                var_dump($pmArray);
-            }
-        } 
+        // var_dump($db['pm'][0]['lastname']); 
     ?>
 
     <!-- rettangoli -->
+    <!-- creeo due div che accoglieranno i nomi dei teacher e dei pm -->
+    <!-- teacher -->
     <div class='lightgray'>
         <?php
-            echo $teacherArray;
-        ?>
+            for($i = 0; $i = $i < count($teacherName); $i++){?>
+            <!-- primo nome -->
+            <div class='name'>
+                <?php echo "Nome:" . ' ' . $teacherName[0]['name'] . '  ' . '  '."Cognome:" . ' '  . $teacherName[0]['lastname'];?>
+            </div>
+                <!-- secondo nome -->
+            <div class='name'>
+                <?php echo "Nome:" . ' ' . $teacherName[$i]['name'] . '  ' . 'Cognome:' . ' ' . $teacherName[$i]['lastname'];?>
+            </div>
+        <?php } ?>
     </div>
 
+    <!-- pm -->
     <div class='green'>
-    <?php
-            echo $pmArray;
-        ?>
+        <?php
+            for($i = 0; $i = $i < count($pmName); $i++){?>
+            <!-- primo nome -->
+            <div class='name'>
+                <?php echo "Nome:" . ' ' . $pmName[0]['name'] . '  ' . '  '."Cognome:" . ' '  . $pmName[0]['lastname'];?>
+            </div>
+                <!-- secondo nome -->
+            <div class='name'>
+                <?php echo "Nome:" . ' ' . $pmName[$i]['name'] . '  ' . 'Cognome:' . ' ' . $pmName[$i]['lastname'];?>
+            </div>
+        <?php } ?>
     </div>
 </body>
 </html>
